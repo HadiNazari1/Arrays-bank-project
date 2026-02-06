a4e5d13495c2918887647413b5e85b2d59b89828
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2026-02-06 10:54:26 +0900

    Update script.js
    challenge #5  completed

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -895,8 +895,10 @@ YOUR TASKS:
 1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion (recFood) and add it to the 
 object as a new property.
 Do NOT create a new array, simply loop over the array (We never did this before, so think about how you can do this without creating a new array).
-2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
-3. Create an array containing all owners of dogs who eat too much (ownersTooMuch) and an array with all owners of dogs who eat too little (ownersTooLittle).
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users, so you first need
+to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much (ownersTooMuch) and an array with all owners of dogs 
+who eat too little (ownersTooLittle).
 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
 5. Log to the console whether there is ANY dog eating EXACTLY the amount of food that is recommended (just true or false)
 6. Log to the console whether ALL of the dogs are eating an OKAY amount of food (just true or false)
@@ -919,7 +921,20 @@ const dogs = [
 
 GOOD LUCK 😀
 */
+  /*
   dogs.forEach(dog=>(dog.recfood=Math.floor(dog.weight**75*28)));
+  const dogSarah = dogs.find(dog=> dog.owners.inculdes('Sarah'));
+  const dogSarah=dogs.find(dog=> dog.owners.includes('Sarah'));
+console.log(`Sarah's dog eats too ${dogSarah.curFood>dogSarah.recfood ? 'much':'little'}`)
+  console.log(`Sara dog's eats too ${dogSarah.curfood> dogSarah.recfood ? 'much' :'little'}`)
+
+  const eatTolittle= dogs.filter(dog=> dog.curFood<dog.recfood).flatMap(dog=> dog.owners)
+
+  const groupedByOwner= Object.groupBy(dogs, dog=> 
+
+  const dogsSorted= dogs.toSorted((a,b) => a.recfood-b.recfood)
+console.log(doggsSored)
+  */
 /*
 const dogs = [
   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
@@ -942,9 +957,7 @@ console.log(
 );
 
 // 3.
-const ownersTooMuch = dogs
-  .filter(dog => dog.curFood > dog.recFood)
-  .flatMap(dog => dog.owners);
+const ownersTooMuch = dogs.filter(dog => dog.curFood > dog.recFood).flatMap(dog => dog.owners);
 const ownersTooLittle = dogs
   .filter(dog => dog.curFood < dog.recFood)
   .flatMap(dog => dog.owners);
@@ -1167,6 +1180,7 @@ cosnt convetTextToTitleCase= function(title){
   const exceptions=['a', 'and', 'or', 'the', 'with', 'in', 'but', 'an']
   const titleCase=title.toLowerCase().split(' ').map(word=> exceptions.includes(word) ? word : word[0].toUpperCase()+word.slice(1)).join(' ');
   const titledCased= title.toLowerCase().split(' ').map(word=> exception.includes(word)? word:word[0].toUpperCase()+word.slice[1]).join(' ');
+
 
 
 
