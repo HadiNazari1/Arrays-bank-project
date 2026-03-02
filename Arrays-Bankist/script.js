c3599da58102ee3c33d909faa8b10f5d3f481a34
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2026-03-02 11:58:03 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -609,21 +609,7 @@ console.log(overalBalance2);
 // Coding Challenge #4
 
 /*
-This time, Julia and Kate are studying the activity levels of different dog breeds.
 
-YOUR TASKS:
-1. Store the the average weight of a "Husky" in a variable "huskyWeight"
-2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
-3. Create an array "allActivities" of all the activities of all the dog breeds
-4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
-5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique 
-array called "swimmingAdjacent".
-6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
-7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
-
-BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
-
-TEST DATA:
 */
 
 /*
@@ -1180,6 +1166,7 @@ cosnt convetTextToTitleCase= function(title){
   const exceptions=['a', 'and', 'or', 'the', 'with', 'in', 'but', 'an']
   const titleCase=title.toLowerCase().split(' ').map(word=> exceptions.includes(word) ? word : word[0].toUpperCase()+word.slice(1)).join(' ');
   const titledCased= title.toLowerCase().split(' ').map(word=> exception.includes(word)? word:word[0].toUpperCase()+word.slice[1]).join(' ');
+
 
 
 
